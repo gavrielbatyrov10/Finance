bd7d59f57b808896f5ac69791e9a3b420330574a
gavriel <gbatyrov10@gmail.com>
2024-11-11 14:59:29 -0500

    navBar: wip

--- src/client/layout/Navbar.jsx
+++ src/client/layout/Navbar.jsx
@@ -1,8 +1,8 @@
 import { useDispatch, useSelector } from "react-redux";
 import { NavLink, useNavigate } from "react-router-dom";
 import { logout, selectToken } from "../features/auth/authSlice";
-
 import "./Navbar.less";
+import { MdOutlineKeyboardDoubleArrowLeft, GrLogout  } from "react-icons/md";
 
 /**
  * A simple navigation bar that displays "Log In" if the user is not logged in,
@@ -11,7 +11,6 @@ import "./Navbar.less";
 export default function Navbar() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
-
   const token = useSelector(selectToken);
 
   const handleLogout = async () => {
@@ -20,22 +19,60 @@ export default function Navbar() {
   };
 
   return (
-    <nav className="top">
-      <h1>Task Tracker</h1>
-      <menu>
-        <li>
-          <NavLink to="/">Home</NavLink>
+    <nav className="navbar">
+      <ul className="navbar-nav">
+        <li className="logo">
+          <NavLink to="/" className="nav-link">
+            <span className="link-text logo-text">Menu</span>
+            <MdOutlineKeyboardDoubleArrowLeft />
+
+          </NavLink>
         </li>
-        {token ? (
-          <li>
-            <a onClick={handleLogout}>Log Out</a>
-          </li>
-        ) : (
-          <li>
-            <NavLink to="/login">Log In</NavLink>
-          </li>
-        )}
-      </menu>
+
+        <li className="nav-item">
+          <NavLink to="/cats" className="nav-link">
+          <MdOutlineKeyboardDoubleArrowLeft />
+
+            <span className="link-text">Cats</span>
+          </NavLink>
+        </li>
+
+        <li className="nav-item">
+          <NavLink to="/aliens" className="nav-link">
+          <MdOutlineKeyboardDoubleArrowLeft />
+
+            <span className="link-text">Aliens</span>
+          </NavLink>
+        </li>
+
+        <li className="nav-item">
+          <NavLink to="/space" className="nav-link">
+            {/* SVG Icon */}
+            <span className="link-text">Space</span>
+          </NavLink>
+        </li>
+
+        <li className="nav-item">
+          <NavLink to="/shuttle" className="nav-link">
+            {/* SVG Icon */}
+            <span className="link-text">Shuttle</span>
+          </NavLink>
+        </li>
+
+        <li className="nav-item">
+          {token ? (
+            <a onClick={handleLogout} className="nav-link">
+              <GrLogout />
+
+              <span className="link-text">Log Out</span>
+            </a>
+          ) : (
+            <NavLink to="/login" className="nav-link">
+              <span className="link-text">Log In</span>
+            </NavLink>
+          )}
+        </li>
+      </ul>
     </nav>
   );
 }
