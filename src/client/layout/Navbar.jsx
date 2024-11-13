5deeadc70b7eca474b0f2a8f6c4a1cea8ef68a55
gavriel <gbatyrov10@gmail.com>
2024-11-13 17:55:12 -0500

    navbar: wip

--- src/client/layout/Navbar.jsx
+++ src/client/layout/Navbar.jsx
@@ -3,7 +3,8 @@ import { NavLink, useNavigate } from "react-router-dom";
 import { logout, selectToken } from "../features/auth/authSlice";
 import "./Navbar.less";
 import { MdOutlineKeyboardDoubleArrowLeft } from "react-icons/md";
-import { GrLogout } from "react-icons/gr"; // Corrected import
+import { GrLogout } from "react-icons/gr";
+import { FaChartLine, FaCity, FaDollarSign, FaEllipsisH, FaSignInAlt, FaHome } from "react-icons/fa"; // Updated icons
 
 /**
  * A navigation bar that displays "Log In" if the user is not logged in,
@@ -30,39 +31,49 @@ export default function Navbar() {
         </li>
 
         <li className="nav-item">
+          <NavLink to="/" className="nav-link">
+            <FaHome /> {/* Icon for Home */}
+            <span className="link-text">Home</span>
+          </NavLink>
+        </li>
+
+        <li className="nav-item">
           <NavLink to="/cats" className="nav-link">
-            <MdOutlineKeyboardDoubleArrowLeft />
-            <span className="link-text">Cats</span>
+            <FaChartLine /> {/* Icon for Stocks */}
+            <span className="link-text">Stocks</span>
           </NavLink>
         </li>
 
         <li className="nav-item">
           <NavLink to="/aliens" className="nav-link">
-            <MdOutlineKeyboardDoubleArrowLeft />
-            <span className="link-text">Aliens</span>
+            <FaCity /> {/* Icon for Real Estate */}
+            <span className="link-text">Real Estate</span>
           </NavLink>
         </li>
 
         <li className="nav-item">
           <NavLink to="/space" className="nav-link">
-            <span className="link-text">Space</span>
+            <FaDollarSign /> {/* Icon for Currency */}
+            <span className="link-text">Currency</span>
           </NavLink>
         </li>
 
         <li className="nav-item">
           <NavLink to="/shuttle" className="nav-link">
-            <span className="link-text">Shuttle</span>
+            <FaEllipsisH /> {/* Icon for More */}
+            <span className="link-text">More</span>
           </NavLink>
         </li>
 
         <li className="nav-item">
           {token ? (
             <a onClick={handleLogout} className="nav-link">
-              <GrLogout />
+              <GrLogout /> {/* Icon for Log Out */}
               <span className="link-text">Log Out</span>
             </a>
           ) : (
             <NavLink to="/login" className="nav-link">
+              <FaSignInAlt /> {/* Icon for Log In */}
               <span className="link-text">Log In</span>
             </NavLink>
           )}
