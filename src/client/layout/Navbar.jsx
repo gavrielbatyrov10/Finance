f58bb2af0affa12e21a94a21f51e6267fcab3714
gavriel <gbatyrov10@gmail.com>
2024-11-17 14:09:24 -0500

    updated nav

--- src/client/layout/Navbar.jsx
+++ src/client/layout/Navbar.jsx
@@ -1,106 +1,90 @@
-// import { useDispatch, useSelector } from "react-redux";
-// import { NavLink, useNavigate } from "react-router-dom";
-// import { logout, selectToken } from "../features/auth/authSlice";
-// import "./Navbar.less";
-// import { useState } from "react";
+import { NavLink, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { logout, selectToken } from "../features/auth/authSlice";
+import { useState } from "react";
+import "./Navbar.less";
 
-// export default function Navbar() {
-//   const dispatch = useDispatch();
-//   const navigate = useNavigate();
-//   const token = useSelector(selectToken);
-//   const [isCurtainOpen, setIsCurtainOpen] = useState(false);
-
-//   const handleLogout = async () => {
-//     await dispatch(logout());
-//     navigate("/");
-//   };
-
-//   const toggleCurtain = () => setIsCurtainOpen(!isCurtainOpen);
-
-//   return (
-//     <nav className="navbar">
-//       <div className="navbar-content">
-//         <NavLink to="/" className="logo">Home</NavLink>
-
-//         {/* Regular nav links for desktop */}
-//         <div className="nav-links">
-//           <NavLink to="/news">News</NavLink>
-//           <div className="dropdown">
-//             <button className="dropbtn">More Links</button>
-//             <div className="dropdown-content">
-//               <NavLink to="/link1">Link 1</NavLink>
-//               <NavLink to="/link2">Link 2</NavLink>
-//               <NavLink to="/link3">Link 3</NavLink>
-//             </div>
-//           </div>
-//           <div className="dropdown">
-//             <button className="dropbtn">Additional Links</button>
-//             <div className="dropdown-content">
-//               <NavLink to="/additional1">Additional Link 1</NavLink>
-//               <NavLink to="/additional2">Additional Link 2</NavLink>
-//               <NavLink to="/additional3">Additional Link 3</NavLink>
-//             </div>
-//           </div>
-//           <div className="auth-link">
-//             {token ? (
-//               <button onClick={handleLogout}>Log Out</button>
-//             ) : (
-//               <NavLink to="/login">Log In</NavLink>
-//             )}
-//           </div>
-//         </div>
-
-//         {/* Hamburger icon for curtain menu on mobile */}
-//         <button className="hamburger" onClick={toggleCurtain}>
-//           &#9776;
-//         </button>
-//       </div>
-
-//       {/* Curtain menu */}
-//       {isCurtainOpen && (
-//         <div className="curtain">
-//           <button className="closebtn" onClick={toggleCurtain}>&times;</button>
-//           <div className="curtain-content">
-//             <NavLink onClick={toggleCurtain} to="/">Home</NavLink>
-//             <NavLink onClick={toggleCurtain} to="/news">News</NavLink>
-//             <NavLink onClick={toggleCurtain} to="/link1">Link 1</NavLink>
-//             <NavLink onClick={toggleCurtain} to="/link2">Link 2</NavLink>
-//             <NavLink onClick={toggleCurtain} to="/link3">Link 3</NavLink>
-//             <NavLink onClick={toggleCurtain} to="/additional1">Additional Link 1</NavLink>
-//             <NavLink onClick={toggleCurtain} to="/additional2">Additional Link 2</NavLink>
-//             <NavLink onClick={toggleCurtain} to="/additional3">Additional Link 3</NavLink>
-//             {token ? (
-//               <button onClick={handleLogout}>Log Out</button>
-//             ) : (
-//               <NavLink onClick={toggleCurtain} to="/login">Log In</NavLink>
-//             )}
-//           </div>
-//         </div>
-//       )}
-//     </nav>
-//   );
-// }
-import React, { useState } from 'react';
- import "./Navbar.less";
+import {  FaArrowDown } from 'react-icons/fa'; // Importing arrow icons
 
 
 export default function Navbar() {
-  const [isOpen, setIsOpen] = useState(false);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const token = useSelector(selectToken);
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
+  const handleLogout = async () => {
+    await dispatch(logout());
+    navigate("/");
+  };
 
   return (
-    <>
-      <div className="hamburger" onClick={() => setIsOpen(!isOpen)}>
-        <div className={isOpen ? 'bar open' : 'bar'}></div>
-        <div className={isOpen ? 'bar open' : 'bar'}></div>
-        <div className={isOpen ? 'bar open' : 'bar'}></div>
+    <nav className="navbar">
+      <div className="logo">
+        <NavLink to="/">MyWebsite</NavLink>
       </div>
-      
-      <div className={`menu ${isOpen ? 'open' : ''}`}>
-        <a href="#home" onClick={() => setIsOpen(false)}>Home</a>
-        <a href="#about" onClick={() => setIsOpen(false)}>About</a>
-        <a href="#services" onClick={() => setIsOpen(false)}>Services</a>
-        <a href="#contact" onClick={() => setIsOpen(false)}>Contact</a>
+
+      {/* Desktop Menu */}
+      <div className={`menu ${isMobileMenuOpen ? "open" : ""}`}>
+        <NavLink to="/" className="menu-item">Home</NavLink>
+
+        {/* About Dropdown menu */}
+        <div className="dropdown">
+          <NavLink to="/about" className="menu-item">About         <FaArrowDown />     </NavLink>
+          <div className="dropdown-content">
+            <NavLink to="/team" className="dropdown-item">Team</NavLink>
+            <NavLink to="/careers" className="dropdown-item">Careers</NavLink>
+            <NavLink to="/history" className="dropdown-item">History</NavLink>
+          </div>
+        </div>
+
+        {/* Services Dropdown menu */}
+        <div className="dropdown">
+          <NavLink to="/services" className="menu-item">Services      <FaArrowDown />     {/* Up Arrow */}
+          </NavLink>
+          <div className="dropdown-content">
+            <NavLink to="/web-design" className="dropdown-item">Web Design</NavLink>
+            <NavLink to="/development" className="dropdown-item">Development</NavLink>
+            <NavLink to="/seo" className="dropdown-item">SEO</NavLink>
+          </div>
+        </div>
+
+        <NavLink to="/contact" className="menu-item">Contact</NavLink>
+
+        {/* Login/Logout link */}
+        {token ? (
+          <button className="menu-item logout-btn" onClick={handleLogout}>Log Out</button>
+        ) : (
+          <NavLink to="/login" className="menu-item login-link">Login</NavLink>
+        )}
       </div>
-    </>
+
+      {/* Hamburger Menu for Mobile */}
+      <button className="hamburger" onClick={toggleMobileMenu}>
+        &#9776;
+      </button>
+
+      {/* Mobile Menu */}
+      {isMobileMenuOpen && (
+        <div className="mobile-menu">
+          <NavLink to="/" className="menu-item" onClick={toggleMobileMenu}>Home 
+          </NavLink>
+          <NavLink to="/about" className="menu-item" onClick={toggleMobileMenu}>About</NavLink>
+          <NavLink to="/services" className="menu-item" onClick={toggleMobileMenu}>Services  &larr;</NavLink>
+          <NavLink to="/contact" className="menu-item" onClick={toggleMobileMenu}>Contact</NavLink>
+
+          {/* Login/Logout in mobile menu */}
+          {token ? (
+            <button className="menu-item logout-btn" onClick={handleLogout}>Log Out</button>
+          ) : (
+            <NavLink to="/login" className="menu-item login-link" onClick={toggleMobileMenu}>Login</NavLink>
+          )}
+        </div>
+      )}
+    </nav>
   );
-};
+}
